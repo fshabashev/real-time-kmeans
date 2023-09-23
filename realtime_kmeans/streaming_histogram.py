64c903e99bff04212d54a124bf7f48e5d0b93cfa
Fedor Shabashev <fedor.shabashev@gmail.com>
2023-09-23 22:06:34 +0100

    minor fix: removing hardcoded value from streaming histogram

--- realtime_kmeans/streaming_histogram.py
+++ realtime_kmeans/streaming_histogram.py
@@ -1,10 +1,11 @@
 class StreamingHistogram:
-    def __init__(self, data_range=(0, 100000), num_bins=100000):
+    def __init__(self, data_range=(0, 100000), num_bins=100000, max_data_points=1400):
         self.data_range = data_range
         self.num_bins = num_bins
         self.bin_width = (data_range[1] - data_range[0]) / num_bins
         self.histogram = [0] * num_bins
         self.queue = []
+        self.max_data_points = max_data_points
 
     def update(self, data_point):
         # Add new data point to the queue
@@ -14,8 +15,8 @@ class StreamingHistogram:
         bin_index = int(data_point / self.bin_width)
         self.histogram[bin_index] += 1
 
-        # If queue has more than 1400 data points, remove the oldest one
-        if len(self.queue) > 1400:
+        # If queue exceeds max_data_points, remove the oldest one
+        if self.max_data_points and len(self.queue) > self.max_data_points:
             old_data_point = self.queue.pop(0)
             # Find the bin for the old data point and decrement its count
             old_bin_index = int(old_data_point / self.bin_width)
