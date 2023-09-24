f53f7cc1853e59e1e9c01653012834932fd3949b
Fedor Shabashev <fedor.shabashev@gmail.com>
2023-09-24 01:44:39 +0100

    minor fix: improving streaming kmeans example

--- examples/realtime_clustering.py
+++ examples/realtime_clustering.py
@@ -26,13 +26,12 @@ if __name__ == "__main__":
     data_size = 10 ** 6
     data_stream = np.random.uniform(0, 100000, data_size)  # Random data points between 0 and 100000
 
-    # Update the histogram with the streaming data in chunks
+    # Update the histogram with the streaming data in chunks and recalculate centroids every 2000 data points
     chunk_size = 1000
     for i in range(0, data_size, chunk_size):
         rt_kmeans.update(data_stream[i:i + chunk_size])
 
-    # Perform clustering
-    labels, centroids = rt_kmeans.cluster()
-
-    # Print results
-    print("Centroids:", centroids)
+        # Recalculate centroids every 5000 data points
+        if (i + chunk_size) % 5000 == 0:
+            _, centroids = rt_kmeans.cluster()
+            print(f"Centroids after processing {i + chunk_size} data points:", centroids)
