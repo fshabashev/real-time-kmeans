934d0a2f6461c61ad85bf9bce20091e0f8d8ba2c
Fedor Shabashev <fedor.shabashev@gmail.com>
2023-09-24 12:22:45 +0100

    adding test to show that histogram based k-means produces the same result as the normal k-means

--- tests/test_kmeans.py
+++ tests/test_kmeans.py
@@ -1,19 +1,39 @@
-from realtime_kmeans.kmeans import weighted_kmeans
+import random
+
 import numpy as np
+from sklearn.cluster import KMeans
+from realtime_kmeans.rt_kmeans import RealTimeKMeans
 
 
-def test_happypath():
-    # Example usage:
-    data = np.array([
-        [1, 2],
-        [5, 6],
-        [1, 1],
-        [5, 5],
-        [2, 2],
-        [6, 6]
-    ])
-    weights = np.array([1, 2, 1, 2, 1, 2])
-    k = 2
+def generate_clustered_data(num_clusters, points_per_cluster, cluster_spread=5000, data_range=(0, 99_000)):
+    centers = np.linspace(data_range[0] + 10000, data_range[1] - 10000, num_clusters)
+    data = []
 
-    labels, centroids = weighted_kmeans(data, weights, k)
-    assert labels is not None
+    for center in centers:
+        data.extend(np.random.normal(center, cluster_spread, points_per_cluster))
+
+    # Clip data to ensure it's within the specified range
+    return np.clip(data, data_range[0], data_range[1])
+
+
+def test_kmeans_similarity():
+    # Generate random data
+    np.random.seed(42)
+
+    # Generate clustered data
+    data = generate_clustered_data(num_clusters=5, points_per_cluster=2000)
+    k = 5
+
+    # Normal KMeans clustering
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(data.reshape(-1, 1))
+    normal_centroids = sorted(kmeans.cluster_centers_.ravel())
+
+    # Histogram-based KMeans clustering
+    rt_kmeans = RealTimeKMeans(data_range=(0, 100000), num_bins=100000, k=k)
+    rt_kmeans.update(data)
+    _, histogram_centroids = rt_kmeans.cluster()
+    histogram_centroids = sorted(histogram_centroids)
+
+    # Compare centroids
+    diff = np.abs(np.array(normal_centroids) - np.array(histogram_centroids))
+    assert np.all(diff < 10), f"Centroids differ by more than tolerance: {diff}"
